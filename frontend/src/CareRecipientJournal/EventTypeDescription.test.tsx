1e14efdaaa43c5b0f542c4c3cd10d3b4e75d661b
Aurel <lighta971@live.fr>
2022-12-16 15:22:33 +0100

    feat(frontend): improve journal title and description

--- frontend/src/CareRecipientJournal/EventTypeDescription.test.tsx
+++ frontend/src/CareRecipientJournal/EventTypeDescription.test.tsx
@@ -6,15 +6,34 @@ describe('EventTypeDescription', () => {
   it.each`
     eventType                     | expectedText
     ${'hadVisit'}                 | ${'Your parents have had visitors today'}
-    ${'hadObservations'}          | ${'Your beloved one is being well taken care of;'}
+    ${'hadObservations'}          | ${'The nurses have made various observations'}
     ${'foodOrFluidIntakeChecked'} | ${`A nurse has been monitoring your loved one's food and`}
     ${'hadMedicationProblem'}     | ${`Your parent may have difficulty taking their medication`}
-    ${'hadAlertOrConcern'}        | ${`Some concern has been raised`}
     ${'hadAlertOrConcern'}        | ${`Some concern has been raised`}
     ${'checkedOut'}               | ${`Your family member has left the care facility`}
     ${'checkedIn'}                | ${`Your parents checked into the care facility and entered his room.`}
     ${'observedMood'}             | ${`We observed your loved one's mood and noticed that he seemed to be`}
   `('renders description for $eventType', ({ eventType, expectedText }) => {
+    // arrange
+    const journalEntry = createJournalEntry()
+
+    // act
+    render(<EventTypeDescription type={eventType} journalEntry={journalEntry} />)
+
+    // assert
+    expect(screen.getByText(new RegExp(expectedText))).toBeInTheDocument()
+  })
+  it.each`
+    eventType                     | expectedText
+    ${'hadVisit'}                 | ${'Visit received'}
+    ${'hadObservations'}          | ${'Observations made'}
+    ${'foodOrFluidIntakeChecked'} | ${`Alimentation observed`}
+    ${'hadMedicationProblem'}     | ${`Medication intake issue observed`}
+    ${'hadAlertOrConcern'}        | ${`Concern raised`}
+    ${'checkedOut'}               | ${`Check-out`}
+    ${'checkedIn'}                | ${`Check-in`}
+    ${'observedMood'}             | ${`Mood observed`}
+  `('renders title for $eventType', ({ eventType, expectedText }) => {
     // arrange
     const journalEntry = createJournalEntry()
 
--- frontend/src/CareRecipientJournal/EventTypeDescription.tsx
+++ frontend/src/CareRecipientJournal/EventTypeDescription.tsx
@@ -33,12 +33,12 @@ type EventTypeDescriptionProps = {
 
 const descriptionDictionnary = {
   hadObservations:
-    'Your beloved one is being well taken care of; the nurses have made various observations, ranging from general observations to those regarding mental health, incontinence pads, and catheters. These observations help the care provider to enhance the quality of life for your beloved one.',
+    'The nurses have made various observations, ranging from general observations to those regarding mental health, incontinence pads, and catheters. These observations help the care provider to enhance the quality of life for your beloved one.',
   hadVisit: 'Your parents have had visitors today.',
   foodOrFluidIntakeChecked:
     "A nurse has been monitoring your loved one's food and liquid intake, as seniors experience a decrease in appetite and sometimes a loss of taste, as well as feeling less thirsty.",
   hadMedicationProblem:
-    'Your parent may have difficulty taking their medication. A nurse will provide follow-up support to help them take it as prescribed.',
+    'Your parent may have difficulty taking their medication. A nurse will provide follow-up support to help him take it as prescribed.',
   hadAlertOrConcern:
     'Some concern has been raised about your family member, and we are looking into it. We will contact you to provide more information.',
   checkedIn: 'Your parents checked into the care facility and entered his room.',
@@ -48,11 +48,11 @@ const descriptionDictionnary = {
 
 const titleDictionnary = {
   hadObservations: 'Observations made',
-  hadVisit: 'Receive visits',
-  foodOrFluidIntakeChecked: 'Alimentation verified',
-  hadMedicationProblem: 'Medication problem',
+  hadVisit: 'Visit received',
+  foodOrFluidIntakeChecked: 'Alimentation observed',
+  hadMedicationProblem: 'Medication intake issue observed',
   hadAlertOrConcern: 'Concern raised',
-  checkedIn: 'Checked-in',
-  checkedOut: 'Checked-out',
-  observedMood: 'Mood',
+  checkedIn: 'Check-in',
+  checkedOut: 'Check-out',
+  observedMood: 'Mood observed',
 }
